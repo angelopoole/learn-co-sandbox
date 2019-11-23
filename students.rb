e8a0f4e4f3a78bd7e8834daf5fde0dc56cf1bdf0
Angelo Poole <angelo.poole@outlook.com>
2019-11-23 23:37:59 +0000

    Automatically backed up by Learn

---
+++ students.rb
@@ -0,0 +1,11 @@
+student1 = "Harry Potter"
+student2 = "Ron Weasley"
+student3 = "Hermione Granger"
+student4 = "Draco Malfoy"
+
+students = [student1, student2, student3, student4]
+
+students[0..2]
+
+# students.rm.last
+push students.rm.last
